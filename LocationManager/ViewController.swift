d0818287d919ea989b425eabe79adaf6b9558fbe
时点软件 冯成林 <Charlin@fengchenglindeiMac.local>
2016-01-19 17:32:36 +0800

    init

---
+++ LocationManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationManager
+//
+//  Created by 冯成林 on 16/1/19.
+//  Copyright © 2016年 冯成林. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
